1241839bba795ba520409d697d88fc911d94ab49
Abel Koech <koechabel161@gmail.com>
2024-08-28 13:28:22 +0300

    comprehensin

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    #pass
+    evens =  [num for num in num_list if num % 2 == 0]
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    #pass
+    sentence=[ sen + '!' for sen in sentence_list] 
+    print(sentence)
+    return sentence
+     
